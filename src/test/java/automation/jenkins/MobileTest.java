393091413564cfff991795eb33c1572a21b06cb2
Goofy <ganes.valliappan72@gmail.com>
2023-07-26 14:09:11 +0530

    initial commit

---
+++ src/test/java/automation/jenkins/MobileTest.java
@@ -0,0 +1,17 @@
+package automation.jenkins;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+@Test
+void login()
+{
+	System.out.println("Login");
+}
+@Test
+void logout()
+{
+	System.out.println("Logout");
+}
+
+}
